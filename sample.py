eba9c41fe365dae5e845c78f57a2f2517f575835
MTHUN021 <mithunwatt@gmail.com>
2021-02-16 17:08:35 +0530

    done

--- sample.py
+++ sample.py
@@ -12,4 +12,17 @@ c1 = complex(1,2)
 c2 = complex(4,5)
 
 c3 = c1 + c2
-print(c3.r)
+
+
+def iptoint(ip):
+    if type(ip) == str:
+        lst = ip.split(".")
+        res = 0
+        l = len(lst)
+        for i in lst:
+            res += pow(256, l-1) * int(i)
+            l -= 1
+
+        return res
+
+print(iptoint("192.168.1.251"))
