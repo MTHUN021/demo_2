b0ee7fc61790ac87841a10e4975714993b5a4c7e
MITHUN021 <43112200+MTHUN021@users.noreply.github.com>
2021-02-21 12:35:20 +0530

    linting

--- assign_4.py
+++ assign_4.py
@@ -32,24 +32,28 @@ class mobile_billing:
 #Bank account class 
 class Bank_account:
 
+
     def __init__(self, account_no, balance, overdraft):
         self.account_no = account_no
         self.balance = balance
         self.overdraft = overdraft
-
         self.interest_rate = 0.04
         self.branch = "Vijaynagar"
     
+
     def credit(self, value):
         self.balance += value
     
+
     def debit(self, value):
         if self.balance <= 0:
             raise Exception
         self.balance -= value
     
+
     def comp_int(self, n):
         self.balance *= (1+self.interest_rate)*n   # n in years
+
 
     def __str__(self):
         return f"No.{self.account_no} with Total Balance(balance - overdraft) = Rs.{self.balance - self.overdraft}"
@@ -58,12 +62,14 @@ class Bank_account:
 # Color class
 class color:
     
+
     def __init__(self, hex_value):
         self.red = hex_value[0]
         self.green = hex_value[1]
         self.blue = hex_value[2]
         self.hex_value = hex_value
     
+
     def form_color(self):
         try:
             return webcolors.rgb_to_name(self.hex_value)
@@ -74,9 +80,11 @@ class color:
 # Point class
 class Point:
 
+
     def __init__(self, x, y):
         self.x = x                # X Co-ordinate
         self.y = y                # Y Co-ordinate
+
 
     def quad(self):               # returns quadrant of the point
         if self.x > 0:
@@ -89,26 +97,34 @@ class Point:
                 return 3
             else:
                 return 4
-            
+
+
     def __str__(self):
         return f"({self.x}, {self.y})"
     
+
     def dist_origin(self):        # returns distance of the point from origin
         return round(pow((self.x**2 + self.y**2), 0.5), 3)
 
 
 #Class Circle/Rectangle/Triangle        
 class circle:
+
+
     def __init__(self, radius):
         self.radius = radius
         
+
     def circumference(self):                           #An example of Polymorphism
         return 2*3.141*self.radius                     #Operator overloading not allowed,  function defined latest/recent will be taken 
     
+
     def area(self):
         return 3.141 * (self.radius ** 2)
 
 class triangle:
+
+
     def __init__(self, a,b,c):
         self.a = a
         self.b = b
@@ -116,41 +132,54 @@ class triangle:
     
         self.s = (self.a + self.b + self.c)/2
 
+
     def area(self):
         return pow(self.s*(self.s-self.a)*(self.s-self.b)*(self.s-self.c), 0.5)
     
+
     def circumference(self):
         return self.a + self.b + self.c
 
 class rectangle():
+
+
     def __init__(self, a,b):
         self.a = a
         self.b = b
 
+
     def area(self):
         return self.a * self.b
     
+
     def circumference(self):
         return 2 * (self.a + self.b)
 
 
 #Class Box
 class box:
+
+
     def __init__(self, length, breadth, height):
         self.length = length
         self.breadth = breadth
         self.height = height
     
+
     def volume(self):
         return self.length * self.breadth * self.height
     
+
     def display(self):
         print(f"Length = {self.length}, Breadth = {self.breadth}, Height = {self.height}")
 
 #class IP address
 class ipaddress:
+
+
     def __init__(self, int_address):
          self.address = int_address
+
 
     def address_str(self):            #converts integer to "a.b.c.d format"
         s = ""
