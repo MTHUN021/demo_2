ae77aa4faadfcf5da2370c399c8314c770497f60
MITHUN021 <43112200+MTHUN021@users.noreply.github.com>
2021-03-10 20:18:48 +0530

    Delete for_reg.py

--- for_reg.py
+++
@@ -1,7 +0,0 @@
-string = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890 @#$%^&*()+ 900-434-2155 800.143.4344 144*124-1558 473/234/4244 424/467/5427 "
-
-import re
-
-print(re.search(r'[98]00[-.]\d{3}[.-]\d{4}', string))
-
-
